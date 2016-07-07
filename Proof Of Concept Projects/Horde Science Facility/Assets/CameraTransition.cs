8a3ec0cf295e2b5d826097b6f1ef866d991be217
Rory Caire-McCaul <rory.cairemcca@ad.aie.edu>
2016-07-07 12:35:49 +0930

    Example of Menu Transtion in Science Facility

---
+++ Proof Of Concept Projects/Horde Science Facility/Assets/CameraTransition.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTransition : MonoBehaviour {
+
+
+    public GameObject camera;
+    Animator controller;
+
+
+    string location;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        controller = camera.GetComponent<Animator>();
+        location = "main";
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (location == "main")
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                controller.SetTrigger("MainToLab");
+                location = "lab";
+            }
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                controller.SetTrigger("MainToBar");
+                location = "bar";
+            }
+        }
+
+
+        if (location == "lab")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                controller.SetTrigger("LabToMain");
+                location = "main";
+            }
+        }
+
+        if (location == "bar")
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                controller.SetTrigger("BarToMain");
+                location = "main";
+            }
+        }
+    }
+}

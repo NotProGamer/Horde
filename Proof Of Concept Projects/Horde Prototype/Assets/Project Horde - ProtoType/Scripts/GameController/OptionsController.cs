5988bf82f32213d89e8af938194489688b02801a
NotProGamer <webreece@gmail.com>
2016-08-01 19:34:05 +0930

    added restart and quit functionality

---
+++ Proof Of Concept Projects/Horde Prototype/Assets/Project Horde - ProtoType/Scripts/GameController/OptionsController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class OptionsController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            // Reload the Current Scene
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+    }
+}

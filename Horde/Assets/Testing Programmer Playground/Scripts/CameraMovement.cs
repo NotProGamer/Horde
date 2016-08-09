3b857350d6023e8ffc68614b04c23201eec03778
NotProGamer <webreece@gmail.com>
2016-08-10 07:32:29 +0930

    playing with camera movement

--- Horde/Assets/Testing Programmer Playground/Scripts/CameraMovement.cs
+++ Horde/Assets/Testing Programmer Playground/Scripts/CameraMovement.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraMovement : MonoBehaviour {
+public class CameraMovement : MonoBehaviour
+{
 
     // note create input controller
 
@@ -16,66 +17,79 @@ public class CameraMovement : MonoBehaviour {
     }
     public State m_state = State.Idle;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+        rigOriginY = transform.position.y;
+
+    }
 
     private Vector3 startPosition;
-    private Vector3 lerpToPosition; 
+    private Vector3 lerpToPosition;
+    private float rigOriginY;
 
-	// Update is called once per frame
-	void Update ()
+    // Update is called once per frame
+    void Update()
     {
 
         Vector3 test = Input.mousePosition;
-        
+
 
         RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(test);
+        Ray ray;
 
         if (Input.GetMouseButtonDown(0))
         {
+            ray = Camera.main.ScreenPointToRay(test);
             if (Physics.Raycast(ray, out hit, 10000))
             {
                 // if you have not selected a rigidbody, then set camera drag start position
                 if (hit.rigidbody == null)
                 {
-                    startPosition = new Vector3(hit.point.x, 0, hit.point.z);
+                    startPosition = new Vector3(hit.point.x, rigOriginY, hit.point.z);
                     m_state = State.Moving;
                 }
             }
         }
-
-        if (m_state == State.Moving)
+        else if (Input.GetMouseButton(0))
         {
-            Vector3 cameraMovement = new Vector3();
-
-            if (Input.GetMouseButton(0))
+            if (m_state == State.Moving)
             {
-                ////transform.position = startPosition;
-                ////m_state = State.Moving;
-                if (Physics.Raycast(ray, out hit, 10000))
-                {
-                    cameraMovement = new Vector3(hit.point.x, 0, hit.point.z);
-                }
-
-                //transform.position = startPosition - cameraMovement;// - cameraMovement;
-
-                lerpToPosition = cameraMovement - startPosition;
-                Debug.Log(lerpToPosition);
-                transform.position = transform.position + startPosition - lerpToPosition;
+                Vector3 test2 = Camera.main.ScreenToWorldPoint(test);
+                test2.y = rigOriginY;
+                transform.position += test2 - startPosition;
             }
+            //Vector3 cameraMovement = new Vector3();
+            //transform.position = startPosition;
+            //ray = Camera.main.ScreenPointToRay(test);
+            //if (Input.GetMouseButton(0))
+            //{
+            //    ////transform.position = startPosition;
+            //    ////m_state = State.Moving;
+            //    if (Physics.Raycast(ray, out hit, 10000))
+            //    {
+            //        cameraMovement = new Vector3(hit.point.x, rigOriginY, hit.point.z);
+            //    }
 
-            if (Input.GetMouseButtonUp(0))
-            {
-                m_state = State.Idle;
+            //    //transform.position = startPosition - cameraMovement;// - cameraMovement;
 
-            }
+            //    lerpToPosition = cameraMovement - startPosition;
+            //    Debug.Log(lerpToPosition);
+            //    transform.position = transform.position + startPosition - lerpToPosition;
+            //}
+
+            //if (Input.GetMouseButtonUp(0))
+            //{
+            //    m_state = State.Idle;
+
+            //}
         }
-
-
-
-
+        else if (Input.GetMouseButtonUp(0))
+        {
+            m_state = State.Idle;
+        }
     }
 }
+
+
+    

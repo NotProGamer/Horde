e1b29bedc4d94a562b0c83b0c756e3277c17e7f7
Reece Howe <reece.howe@ad.aie.edu>
2016-08-09 16:38:35 +0930

    updated test level and modified zombie prefab

---
+++ Horde/Assets/Testing Programmer Playground/Scripts/CameraMovement.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+    // note create input controller
+
+    // click to generate noise
+
+    // click and drag to move camera
+
+    public enum State
+    {
+        Idle,
+        Moving
+    }
+    public State m_state = State.Idle;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    private Vector3 startPosition;
+
+
+	// Update is called once per frame
+	void Update ()
+    {
+
+        Vector3 test = Input.mousePosition;
+        
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(test);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out hit, 10000))
+            {
+                // if you have not selected a rigidbody, then set camera drag start position
+                if (hit.rigidbody == null)
+                {
+                    startPosition = hit.point;
+                    m_state = State.Moving;
+                }
+            }
+        }
+        Vector3 cameraMovement = new Vector3();
+
+        if (Input.GetMouseButton(0))
+        {
+            transform.position = startPosition;
+            //m_state = State.Moving;
+            if (Physics.Raycast(ray, out hit, 10000))
+            {
+                cameraMovement = hit.point;
+            }
+            
+            transform.position += cameraMovement - startPosition;// - cameraMovement;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            m_state = State.Idle;
+        }
+
+
+
+    }
+}

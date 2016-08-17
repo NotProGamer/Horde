e58af992d52536ff440b4692c14f00c75a8d5ab7
Reece Howe <reece.howe@ad.aie.edu>
2016-08-17 12:47:28 +0930

    making changes to the noise manger script

--- Horde/Assets/Testing Programmer Playground/Scripts/NoiseManager.cs
+++ Horde/Assets/Testing Programmer Playground/Scripts/NoiseManager.cs
@@ -3,88 +3,111 @@ using System.Collections;
 using System.Collections.Generic;
 
 [System.Serializable]
-public enum NoisePriority
+//public enum NoisePriority
+//{
+//    NoPriority, // Ignored by Zombie
+//    LowestPriority, // Amibent
+//    LowPriority, // Other , car alarm etc.
+//    MediumPrioritty, // Human
+//    HighPriority, // User Taps
+//    MaxPriority, // GameOverride
+//}
+public enum NoiseIdentifier
 {
-    NoPriority, // Ignored by Zombie
-    LowestPriority, // Amibent
-    LowPriority, // Other , car alarm etc.
-    MediumPrioritty, // Human
-    HighPriority, // User Taps
-    MaxPriority, // GameOverride
+    UserTap,
+    Human,
+    Expolosion,
+    Alarm,
+    WeaponFire,
+    Zombie,
+    Silent
 }
 
 public class Noise
 {
-    public Vector3 m_position;
-    public float m_volume; // is like a range or radius of the sound
-    public float m_reductionOverTime;
-    private NoisePriority m_priority = NoisePriority.NoPriority;
-    //private float m_timeStamp = 0f;
-
+    public Vector3 m_position = new Vector3();
+    public float m_volume = 0f; // is like a range or radius of the sound
+    public NoiseIdentifier m_identifier = NoiseIdentifier.Silent;
+    public float m_expiry = 0f;
+    //public float m_reductionOverTime;
+    //private NoisePriority m_priority = NoisePriority.NoPriority;
     //private static int m_id = 0;
     //public int GetID()
     //{
     //    return m_id;
     //}
 
-    public Noise(Vector3 position, float volume, float reduction, NoisePriority priority = NoisePriority.NoPriority)
+    public Noise(Vector3 position, float volume, float expirationDelay, NoiseIdentifier identifier = NoiseIdentifier.Silent)
     {
         m_position = position;
         m_volume = volume;
-        m_reductionOverTime = reduction;
-        m_priority = priority;
-        //m_timeStamp = Time.time;
+        m_identifier = identifier;
+        m_expiry = Time.time + expirationDelay;
         //m_id += 1; 
+    }
+
+    public bool IsExpired()
+    {
+        return Time.time > m_expiry;
+    }
+
+    public NoiseIdentifier Identifier()
+    {
+        return m_identifier;
     }
 
     public float CalculateAudabilityFromPosition(Vector3 audiencePosition)
     {
         float audible = 0f;
-        float distance = (m_position - audiencePosition).magnitude;
-        if (m_volume > distance)
-        {
-            // can be heard
-            audible = m_volume - distance;
-            
-        }
+        float sqrDistance = (m_position - audiencePosition).sqrMagnitude;
+        if ()
 
-        //float sqrDistance = (m_position - audiencePosition).sqrMagnitude;
-        //float sqrVolume = m_volume * m_volume;
-        //if (sqrVolume > sqrDistance)
-        //{
-        //    // can be heard
-        //    audible = sqrVolume - sqrDistance;
-
-        //}
-
-        if (audible < 0)
-        {
-            audible = 0;
-        }
-        //audible = m_volume / (0.1f + distance);
-        //Debug.Log(sqrVolume + " - " + sqrDistance + " = " + audible);
         return audible;
     }
 
-    public void Update(float deltaTime)
-    {
-        if (m_volume > 0)
-        {
-            m_volume -= m_reductionOverTime * deltaTime;
-        }
-        if (m_volume < 0)
-        {
-            m_volume = 0;
-        }
-    }
-    public bool IsExpired()
-    {
-        return m_volume == 0;
-    }
-    public NoisePriority Priority()
-    {
-        return m_priority;
-    }
+    //public float CalculateAudabilityFromPosition(Vector3 audiencePosition)
+    //{
+    //    float audible = 0f;
+    //    float distance = (m_position - audiencePosition).magnitude;
+    //    if (m_volume > distance)
+    //    {
+    //        // can be heard
+    //        audible = m_volume - distance;
+    //    }
+    //    //float sqrDistance = (m_position - audiencePosition).sqrMagnitude;
+    //    //float sqrVolume = m_volume * m_volume;
+    //    //if (sqrVolume > sqrDistance)
+    //    //{
+    //    //    // can be heard
+    //    //    audible = sqrVolume - sqrDistance;
+    //    //}
+    //    if (audible < 0)
+    //    {
+    //        audible = 0;
+    //    }
+    //    //audible = m_volume / (0.1f + distance);
+    //    //Debug.Log(sqrVolume + " - " + sqrDistance + " = " + audible);
+    //    return audible;
+    //}
+    //public void Update(float deltaTime)
+    //{
+    //    if (m_volume > 0)
+    //    {
+    //        m_volume -= m_reductionOverTime * deltaTime;
+    //    }
+    //    if (m_volume < 0)
+    //    {
+    //        m_volume = 0;
+    //    }
+    //}
+    //public bool IsExpired()
+    //{
+    //    return m_volume == 0;
+    //}
+    //public NoisePriority Priority()
+    //{
+    //    return m_priority;
+    //}
 }
 
 public class NoiseManager : MonoBehaviour {
@@ -100,9 +123,9 @@ public class NoiseManager : MonoBehaviour {
             return m_noises.Count;
         }
 
-        public Noise Add(Vector3 position, float volume, float reduction, NoisePriority priority = NoisePriority.NoPriority)
+        public Noise Add(Vector3 position, float volume, float expirationDelay, NoiseIdentifier identifier = NoiseIdentifier.Silent)
         {
-            Noise noise = new Noise(position, volume, reduction, priority);
+            Noise noise = new Noise(position, volume, expirationDelay, identifier);
             m_noises.Add(noise);
             return noise;
         }
---
+++ Horde/Assets/Testing Programmer Playground/Scripts/Scaler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scaler : MonoBehaviour {
+
+    public float m_scale = 1f;
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.localScale = new Vector3(m_scale, m_scale, m_scale);
+	}
+}

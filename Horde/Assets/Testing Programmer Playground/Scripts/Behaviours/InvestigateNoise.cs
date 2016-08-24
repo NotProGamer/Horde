60c7cccca416f416a44d5ff6a2b15ed88d2e7f60
Reece Howe <reece.howe@ad.aie.edu>
2016-08-24 16:20:45 +0930

    moved behaviours folder

---
+++ Horde/Assets/Testing Programmer Playground/Scripts/Behaviours/InvestigateNoise.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InvestigateNoise : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

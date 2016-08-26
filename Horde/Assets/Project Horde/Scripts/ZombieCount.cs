36d65924d0164a544a28d07ca592f2ae4f52b181
AIE\rory.cairemcca <rory.rangersapprentice>
2016-08-26 16:59:27 +0930

    Added UI Buttons

---
+++ Horde/Assets/Project Horde/Scripts/ZombieCount.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ZombieCount : MonoBehaviour 
+{
+    public Text hordeCount;
+   
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

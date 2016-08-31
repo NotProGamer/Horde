12c36451a4601cefe7fa7f107a2fc632bba986cb
AIE\rory.cairemcca <rory.rangersapprentice>
2016-08-31 14:31:16 +0930

    Worked on Level 2 Added UI Folder

--- Horde/Assets/Project Horde/Scripts/ZombieCount.cs
+++ Horde/Assets/Project Horde/Scripts/ZombieCount.cs
@@ -5,17 +5,36 @@ using UnityEngine.UI;
 public class ZombieCount : MonoBehaviour 
 {
     public Text hordeCount;
-   
+    int hordeSizeInt;
+	GameObject[] hordeArray;
+	private float m_delay = 1.0f;
+	private float nextTime = 0f;
 
 
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+    {
+		
+		if (Time.time > nextTime) 
+		{
+			
+			hordeArray = GameObject.FindGameObjectsWithTag ("Player");
+
+			if (hordeArray != null) 
+			{
+				hordeSizeInt = hordeArray.Length;
+			}
+
+			hordeCount.text = hordeSizeInt.ToString();
+			nextTime = Time.time + m_delay;
+		}
+
 	}
 }

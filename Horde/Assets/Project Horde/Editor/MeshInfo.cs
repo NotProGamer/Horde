dab6676abc996ffc2a7edc42ad37d1be55b43fd1
Rory Caire-McCaul <rory.rangersapprentice@gmail.com>
2016-10-10 18:01:07 +1030

    Deleted air conditioners from building prefabs Added zombie spawn button

---
+++ Horde/Assets/Project Horde/Editor/MeshInfo.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class MeshInfo : EditorWindow
+{
+
+    private int vertexCount;
+    private int submeshCount;
+    private int triangleCount;
+
+    [MenuItem("Tools/Mesh Info")]
+    static void Init()
+    {
+        // Get existing open window or if none, make a new one:
+        MeshInfo window = (MeshInfo)EditorWindow.GetWindow(typeof(MeshInfo));
+        window.titleContent.text = "Mesh Info";
+    }
+
+    void OnSelectionChange()
+    {
+        Repaint();
+    }
+
+
+
+    void OnGUI()
+    {
+        if (Selection.activeGameObject && Selection.activeGameObject.GetComponent<MeshFilter>())
+        {
+            vertexCount = Selection.activeGameObject.GetComponent<MeshFilter>().sharedMesh.vertexCount;
+            triangleCount = Selection.activeGameObject.GetComponent<MeshFilter>().sharedMesh.triangles.Length / 3;
+            submeshCount = Selection.activeGameObject.GetComponent<MeshFilter>().sharedMesh.subMeshCount;
+
+            EditorGUILayout.LabelField(Selection.activeGameObject.name);
+            EditorGUILayout.LabelField("Vertices: ", vertexCount.ToString());
+            EditorGUILayout.LabelField("Triangles: ", triangleCount.ToString());
+            EditorGUILayout.LabelField("SubMeshes: ", submeshCount.ToString());
+        }
+    }
+
+}

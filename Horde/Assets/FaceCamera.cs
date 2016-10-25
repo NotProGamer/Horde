a5360bcf065ddf8a131b268270b18af169f42f4b
AIE\rory.cairemcca <rory.rangersapprentice>
2016-10-25 16:38:46 +1030

    Added FaceCamera Script for floating UI elements Updated HumanCounter Script to display floating icon above survivors after a % of humans are killed in a safe zone

---
+++ Horde/Assets/FaceCamera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceCamera : MonoBehaviour
+{
+    public Transform target;
+
+
+    void Awake()
+    {
+        target = Camera.main.transform;
+    }
+
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (target != null)
+        {
+            transform.LookAt(target);   
+        }
+    }
+}

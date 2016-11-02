2cf3019d35dbab520b9bfdbfc67663a448260d1f
AIE\rory.cairemcca <rory.rangersapprentice>
2016-11-02 16:32:53 +1030

    Created credits scene. Updated Menu Zombie prefab

---
+++ Horde/Assets/MenuButtons.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButtons : MonoBehaviour
+{
+    public GameObject[] buttons;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+
+
+    public void ShowButtons()
+    {
+        foreach (GameObject obj in buttons)
+        {
+            obj.SetActive(true);
+        }
+    }
+
+
+}

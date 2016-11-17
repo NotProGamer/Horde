5e6bdccab1c593388160342050d2300294311fa7
Reece Howe <reece.howe@ad.aie.edu>
2016-11-17 14:33:32 +1030

    test push

---
+++ Horde/Assets/Testing Programmer Playground/Scripts/UISpawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class UISpawner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

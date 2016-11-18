e238431d79c05eba750805aa230f38c578510379
AIE\rory.cairemcca <rory.rangersapprentice>
2016-11-18 16:52:30 +1030

    Safe Zone Information Script - Changes color of SafeZone aura when completed Added WinCamPan script - In progress - pans camera view to target (used on last kill) Duplicated Level 1 and Created Level 1 SafeZoneTest Added shield image

--- Horde/Assets/Project Horde/Scripts/SafeZoneInformation.cs
+++ Horde/Assets/Project Horde/Scripts/SafeZoneInformation.cs
@@ -5,17 +5,53 @@ public class SafeZoneInformation : MonoBehaviour
 {
     ObjectiveUpdater objective;
 
-    
+    public Renderer[] safeZoneIndicators;
+
+    float duration = 1.0f;
+    float alpha = 0;
+
+
 
     // Use this for initialization
     void Start ()
     {
-        objective = GetComponent<ObjectiveUpdater>();	
+        objective = GetComponent<ObjectiveUpdater>();
+
+        safeZoneIndicators = gameObject.GetComponentsInChildren<Renderer>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-	
-	}
+        if (objective.m_status == ObjectiveStatus.Incomplete)
+        {
+            foreach (Renderer rend in safeZoneIndicators)
+            {
+                rend.material.color = Color.blue;
+            }
+        }
+        else if (objective.m_status == ObjectiveStatus.Complete)
+        {
+            foreach (Renderer rend in safeZoneIndicators)
+            {
+                rend.material.color = Color.green;
+            }       
+        }
+
+        LerpAlpha();
+    }
+
+
+    void LerpAlpha()
+    {
+        float lerp = Mathf.PingPong(Time.time, duration) / duration;
+        alpha = Mathf.Lerp(0.0f, 1.0f, lerp);
+
+        foreach (Renderer rend in safeZoneIndicators)
+        {
+            Color col = rend.material.color;
+            col.a = alpha;
+            rend.material.color = col;
+        }
+    }
 }
---
+++ Horde/Assets/WinCamPan.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinCamPan : MonoBehaviour
+{
+
+    //    public bool panTheCam = false;
+
+
+
+    public GameObject looktarget;
+    Health hp = null;
+
+
+    float desiredFov = 10;
+    float currentFov;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        hp = looktarget.GetComponent<Health>();
+        currentFov = Camera.main.fieldOfView;
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (hp.IsDead())
+        {
+            iTween.LookTo(gameObject, looktarget.transform.position, 2.0f);
+            if (currentFov != desiredFov)
+            {
+                Camera.main.fieldOfView -= 1;
+                currentFov = Camera.main.fieldOfView;
+            }
+
+
+            
+
+            
+            //iTween.MoveAdd(gameObject, Vector3 amount, float time)
+        }
+	
+	}
+}

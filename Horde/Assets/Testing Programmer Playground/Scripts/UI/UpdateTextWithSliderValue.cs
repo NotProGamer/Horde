1a8890c906f4ee0044fd0cfa4b7ad84212f7345b
Reece Howe <reece.howe@ad.aie.edu>
2016-11-23 13:57:58 +1030

    new scenes for UI and loading menu

---
+++ Horde/Assets/Testing Programmer Playground/Scripts/UI/UpdateTextWithSliderValue.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UpdateTextWithSliderValue : MonoBehaviour {
+
+    public GameObject m_slider = null;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (m_slider)
+        {
+            GetComponent<Text>().text = m_slider.GetComponent<Slider>().value.ToString();
+        }
+	    
+	}
+}

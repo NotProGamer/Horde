6418ac6811016920e8d1b12bb58ac74874f14ca6
Reece Howe <reece.howe@ad.aie.edu>
2016-11-24 14:14:51 +1030

    starting to make an object spawner

---
+++ Horde/Assets/Testing Programmer Playground/Scripts/ObjectSpawner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectSpawner : MonoBehaviour {
+
+    // create an object spawne that spawn object from the object pool
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

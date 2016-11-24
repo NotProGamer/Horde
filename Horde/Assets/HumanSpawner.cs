7dc6c1d1fb5c3a326b866ec894013948023fc1ac
AIE\rory.cairemcca <rory.rangersapprentice>
2016-11-24 14:12:48 +1030

    Added temporary HumanSpawner Added spawner to level 1 Spawnpoint prefab

---
+++ Horde/Assets/HumanSpawner.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HumanSpawner : MonoBehaviour
+{
+    ObjectPoolManager objPool;
+
+
+    public Transform[] spawnPoints;
+    public int spawnCap;
+
+
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        foreach (Transform spawner in spawnPoints)
+            {
+                SpawnPoint spawnPoint = spawner.GetComponent<SpawnPoint>();
+                spawnPoint.SpawnHuman("Human");
+            }  
+        }
+
+    }
+
+
+    //void SpawnHuman (string identifier, Transform sp)
+    //{
+    //    if (objPool != null)
+    //    {
+    //        objPool.RequestObjectAtPosition(identifier, sp.position);
+    //        Debug.Log("spawned a dude");
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("no spawner");
+    //    }
+        
+    //}
+
+
+
---
+++ Horde/Assets/SpawnPoint.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPoint : MonoBehaviour
+{
+    ObjectPoolManager objPool;
+
+    public float spawnWaitTime;
+    float timeSinceLastSpawn;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        GameObject gameController = GameObject.FindGameObjectWithTag("GameController");
+        if (gameController != null)
+        {
+            objPool = gameController.GetComponent<ObjectPoolManager>();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void SpawnHuman(string identifier)
+    {
+        if (objPool != null)
+        {
+            if (Time.time - spawnWaitTime >= timeSinceLastSpawn)
+            {
+                timeSinceLastSpawn = Time.time;
+                objPool.RequestObjectAtPosition(identifier, transform.position);
+                Debug.Log("spawned a dude");
+            }
+        }
+        else
+        {
+            Debug.Log("no spawner");
+        }
+
+    }
+}
